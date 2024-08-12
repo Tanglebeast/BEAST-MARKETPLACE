c70881b73d86909c715172b3e07f36e83f080538
Tanglebeast <marvguellier1999@gmail.com>
2024-08-12 22:13:17 +0200

    mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/Header.css';
+import '../styles/Header.css';  // Verweisen Sie auf Ihre einzige CSS-Datei
 import ShortenAddress from './ShortenAddress';
 import { getUserName, getProfilePicture, initializeMarketplace } from '../components/utils';
 import NetworkselectionDropdown from './NetworkselectionDropdown';
@@ -11,6 +11,7 @@ const Header = ({ isConnected, account, connectWallet, disconnectWallet }) => {
   const [profilePicture, setProfilePicture] = useState('/placeholder-PFP.png');
   const [marketplace, setMarketplace] = useState(null);
   const [isInitializing, setIsInitializing] = useState(true);
+  const [showMobileMenu, setShowMobileMenu] = useState(false); // Zustand für das Hamburger-Menü
 
   useEffect(() => {
     const setupMarketplace = async () => {
@@ -61,45 +62,56 @@ const Header = ({ isConnected, account, connectWallet, disconnectWallet }) => {
   return (
     <header>
       <nav className="navbar">
-        <a href="/" className="title">
-          <img src="/fractalz-logo-black.svg" alt="NFT Marketplace Logo" className="logo" />
-        </a>
-        <div className="nav-links">
-          <a href="/collections" className="nav-link">GALLERY</a>
-          <a href="/artists" className="nav-link">ARTISTS</a>
-          <a href="/users" className="nav-link">USERS</a>
-          <a href="/fairmint" className="nav-link">FAIRMINT</a>
-          <a href="/about" className="nav-link">ABOUT</a>
-        </div>
-        <NetworkselectionDropdown />
-        {isInitializing ? (
-          <img src='/basic-loading.gif' alt='loading-spinner' className="loading-spinner" />
-        ) : isConnected ? (
-          <div className="connected-container flex center-ho"
-               onMouseEnter={handleMouseEnter}
-               onMouseLeave={handleMouseLeave}>
-            <div className='PFPImage flex centered'>
-              <img 
-                src={profilePicture} 
-                alt="Profile" 
-                className="profile-picture" 
-              />
+        <div className="nav-container">
+          <a href="/" className="title">
+            <img src="/fractalz-logo-black.svg" alt="NFT Marketplace Logo" className="logo" />
+          </a>
+          <button className="hamburger" onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </button>
+          <div className={`nav-links ${showMobileMenu ? 'active' : ''}`}>
+            <div className='ml200'>
+            <a href="/collections" className="nav-link">GALLERY</a>
+            <a href="/artists" className="nav-link">ARTISTS</a>
+            <a href="/users" className="nav-link">USERS</a>
+            <a href="/fairmint" className="nav-link">FAIRMINT</a>
+            <a href="/about" className="nav-link">ABOUT</a>
             </div>
-            <p className="account">
-              {username ? username : <ShortenAddress address={account} />}
-            </p>
-            {showDropdown && (
-              <div className="dropdown">
-                <div className="dropdown-hover">
-                  <a href="/wallet" className="dropdown-link">MY ACCOUNT</a>
+            <div className='flex centered burder-account-network'>
+            <NetworkselectionDropdown />
+            {isInitializing ? (
+              <img src='/basic-loading.gif' alt='loading-spinner' className="loading-spinner" />
+            ) : isConnected ? (
+              <div className="connected-container flex center-ho"
+                   onMouseEnter={handleMouseEnter}
+                   onMouseLeave={handleMouseLeave}>
+                <div className='PFPImage flex centered'>
+                  <img 
+                    src={profilePicture} 
+                    alt="Profile" 
+                    className="profile-picture" 
+                  />
                 </div>
-                <button className="button-disconnect" onClick={handleDisconnect}>DISCONNECT</button>
+                <p className="account">
+                  {username ? username : <ShortenAddress address={account} />}
+                </p>
+                {showDropdown && (
+                  <div className="dropdown">
+                    <div className="dropdown-hover">
+                      <a href="/wallet" className="dropdown-link">MY ACCOUNT</a>
+                    </div>
+                    <button className="button-disconnect" onClick={handleDisconnect}>DISCONNECT</button>
+                  </div>
+                )}
               </div>
+            ) : (
+              <button className="button" onClick={connectWallet}>CONNECT</button>
             )}
+            </div>
           </div>
-        ) : (
-          <button className="button" onClick={connectWallet}>CONNECT</button>
-        )}
+        </div>
       </nav>
     </header>
   );
