88d5b6d98832ffdea50e432fd8a42665161d2b55
Tanglebeast <marvguellier1999@gmail.com>
2024-09-29 16:58:51 +0200

    test deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['web3'],
+    // oder falls Probleme bestehen:
+    // exclude: ['web3']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/web3/],
+    },
+  },
+});
