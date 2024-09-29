739992edff7c1f403fc94f2999a311db361839f7
Tanglebeast <marvguellier1999@gmail.com>
2024-09-29 18:18:30 +0200

    dep test

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'web3': require.resolve('web3/dist/web3.min.js')
-    }
-  },
-  optimizeDeps: {
-    exclude: ['web3']
-  }
 })
