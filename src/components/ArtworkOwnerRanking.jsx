b4b6b3c515f16914aff72a8042f47be87604a933
Tanglebeast <marvguellier1999@gmail.com>
2024-09-30 19:11:19 +0200

    artworkownerranking loading

--- src/components/ArtworkOwnerRanking.jsx
+++ src/components/ArtworkOwnerRanking.jsx
@@ -9,6 +9,7 @@ const web3 = new Web3(window.ethereum);
 const ArtworkOwnerRanking = ({ collectionAddress, marketplace }) => {
   const [ownerData, setOwnerData] = useState([]);
   const [totalSupply, setTotalSupply] = useState(0);
+  const [loading, setLoading] = useState(true); // Ladezustand hinzufügen
 
   useEffect(() => {
     const fetchOwnerData = async () => {
@@ -47,6 +48,8 @@ const ArtworkOwnerRanking = ({ collectionAddress, marketplace }) => {
         setOwnerData(sortedOwners);
       } catch (error) {
         console.error('Error fetching owner data:', error);
+      } finally {
+        setLoading(false); // Ladezustand beenden
       }
     };
 
@@ -56,27 +59,33 @@ const ArtworkOwnerRanking = ({ collectionAddress, marketplace }) => {
   return (
     <div className="artwork-owner-ranking">
       <h2 className='mt5'>Top Owners</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Owner</th>
-            <th>Owned</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ownerData.map(({ owner, username, profilePicture, count, percentage }) => (
-            <tr key={owner}>
-              <td className='VisibleLink'>
-                <a className='OwnerInfoDiv' href={`/users/${owner}`}>
-                  <img src={profilePicture} alt={username} className="owner-profile-picture" />
-                  {username || <ShortenAddress address={owner} />}
-                </a>
-              </td>
-              <td>{count} NFTs</td>
+      {loading ? (
+        <div className="loading-container">
+          <img src="/basic-loading.gif" alt="Loading..." className="loading-gif" />
+        </div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Owner</th>
+              <th>Owned</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {ownerData.map(({ owner, username, profilePicture, count, percentage }) => (
+              <tr key={owner}>
+                <td className='VisibleLink'>
+                  <div className='OwnerInfoDiv padding2'>
+                    <img src={profilePicture} alt={username} className="owner-profile-picture" />
+                    <a href={`/users/${owner}`} className='pointer'>{username || <ShortenAddress address={owner} />}</a>
+                  </div>
+                </td>
+                <td>{count} NFTs</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
