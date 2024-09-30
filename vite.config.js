67661da7ca4efe70c9a0a85807683baaf9635fe7
Tanglebeast <marvguellier1999@gmail.com>
2024-09-30 10:24:18 +0200

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    proxy: {
+      '/ipfs': {
+        target: 'https://ipfs.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ipfs/, '')
+      }
+    }
+  }
+}
+
